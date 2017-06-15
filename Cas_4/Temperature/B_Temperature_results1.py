635f3e569683c8a3a903a4c6b5738ba4adc722dc
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-06-15 14:52:11 +0200

    Create B_Temperature_results1.py

---
+++ Cas_4/Temperature/B_Temperature_results1.py
@@ -0,0 +1,11 @@
+# First Iteration
+
+![alt tab](https://user-images.githubusercontent.com/26437161/27182009-1f708206-51da-11e7-9038-a3df6bdddecc.png)
+
+# Last Iteration
+
+![alt tab](https://user-images.githubusercontent.com/26437161/27182011-1f77d704-51da-11e7-96c5-00e17b3ac620.png)
+
+# Movie
+
+![alt tab](https://user-images.githubusercontent.com/26437161/27182010-1f77419a-51da-11e7-8101-4d381e975a3b.gif)
