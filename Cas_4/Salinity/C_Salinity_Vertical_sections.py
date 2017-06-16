5d291d6d2c744688bfe3c0c1e871b33f3e93c730
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-06-16 11:24:28 +0200

    Update C_Salinity_Vertical_sections.py

--- Cas_4/Salinity/C_Salinity_Vertical_sections.py
+++ Cas_4/Salinity/C_Salinity_Vertical_sections.py
@@ -10,37 +10,25 @@ dir1 = '/homedata/bderembl/runmit/test_southatlgyre3'
 
 ds1 = open_mdsdataset(dir1,iters='all',prefix=['S'])
 
-nt = -1                                 #Last iteration
-nx = np.array(len(ds1['S'].XC)/2)                                 
-ny = np.array(len(ds1['S'].YC)/2)
+Height = ds1.S.Z
+print(Height)
 
-print(nx)  #280
-print(ny)  #100
+nx = len(ds1.S.XC)/2
+print(nx)
+ny = len(ds1.S.YC)/2
+print(ny)
+
+nt = -1
 
 # Vertical Section of Salinity
-
 plt.figure(1)
-ax = plt.subplot(projection=ccrs.PlateCarree());
-ds1['S'].where(ds1.hFacC>0)[nt,:,:,100].plot.pcolormesh('XC','YC',ax=ax)
-plt.title('Case 4 : Salinity (t = -1 ; YC = 30°S)')
-ax.coastlines()
-gl = ax.gridlines(draw_labels=True, alpha = 0.5, linestyle='--');
-gl.xlabels_top = False
-gl.ylabels_right = False
-gl.xformatter = LONGITUDE_FORMATTER
-gl.yformatter = LATITUDE_FORMATTER
+ds1['S'].where(ds1.hFacC>0)[nt,:,100,:].plot()
+plt.title('Case 4 : Salinity (t=-1 ; YC = 30S)')
 plt.savefig('S_Salinity_Vertical_section(x,z)_cas4'+'.png')
 plt.clf()
 
 plt.figure(2)
-ax = plt.subplot(projection=ccrs.PlateCarree());
-ds1['S'].where(ds1.hFacC>0)[nt,:,280,:].plot.pcolormesh('XC','YC',ax=ax)
-plt.title('Case 4 : Salinity (t = -1 ; XC = 0°E)')
-ax.coastlines()
-gl = ax.gridlines(draw_labels=True, alpha = 0.5, linestyle='--');
-gl.xlabels_top = False
-gl.ylabels_right = False
-gl.xformatter = LONGITUDE_FORMATTER
-gl.yformatter = LATITUDE_FORMATTER
+ds1['S'].where(ds1.hFacC>0)[nt,:,:,280].plot()
+plt.title('Case 4 : Salinity (t=-1 ; XC = 0E)')
 plt.savefig('S_Salinity_Vertical_section(y,z)_cas4'+'.png')
 plt.clf()
