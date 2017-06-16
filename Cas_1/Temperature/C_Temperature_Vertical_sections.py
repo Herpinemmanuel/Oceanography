3d331ecdb9cb0e64050eb3e4ece27242e1714b3e
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-06-16 16:47:38 +0200

    Update C_Temperature_Vertical_sections.py

--- Cas_1/Temperature/C_Temperature_Vertical_sections.py
+++ Cas_1/Temperature/C_Temperature_Vertical_sections.py
@@ -22,11 +22,11 @@ nt = -1
 plt.figure(1)
 ds1['T'].where(ds1.hFacC>0)[nt,:,ny,:].plot()
 plt.title('Case 1 : Temperature (t=-1 ; YC = 30S)')
-plt.savefig('T_Temperature_Vertical_section_xz_cas4'+'.png')
+plt.savefig('T_Temperature_Vertical_section_xz_cas1'+'.png')
 plt.clf()
 
 plt.figure(2)
 ds1['T'].where(ds1.hFacC>0)[nt,:,:,nx].plot()
 plt.title('Case 1 : Temperature (t=-1 ; XC = 0E)')
-plt.savefig('T_Temperature_Vertical_section_yz_cas4'+'.png')
+plt.savefig('T_Temperature_Vertical_section_yz_cas1'+'.png')
 plt.clf()
