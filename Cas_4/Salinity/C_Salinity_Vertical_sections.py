777e980296e0bbe6b4bd71cd1c19197aa07e2b10
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-06-16 16:28:38 +0200

    Update C_Salinity_Vertical_sections.py

--- Cas_4/Salinity/C_Salinity_Vertical_sections.py
+++ Cas_4/Salinity/C_Salinity_Vertical_sections.py
@@ -11,22 +11,22 @@ ds1 = open_mdsdataset(dir1,iters='all',prefix=['S'])
 Height = ds1.S.Z
 print(Height)
 
-nx = len(ds1.S.XC)/2
+nx = int(len(ds1.S.XC)/2)
 print(nx)
-ny = len(ds1.S.YC)/2
+ny = int(len(ds1.S.YC)/2)
 print(ny)
 
 nt = -1
 
 # Vertical Section of Salinity
 plt.figure(1)
-ds1['S'].where(ds1.hFacC>0)[nt,:,100,:].plot()
+ds1['S'].where(ds1.hFacC>0)[nt,:,ny,:].plot()
 plt.title('Case 4 : Salinity (t=-1 ; YC = 30S)')
 plt.savefig('S_Salinity_Vertical_section_xz_cas4'+'.png')
 plt.clf()
 
 plt.figure(2)
-ds1['S'].where(ds1.hFacC>0)[nt,:,:,280].plot()
+ds1['S'].where(ds1.hFacC>0)[nt,:,:,nx].plot()
 plt.title('Case 4 : Salinity (t=-1 ; XC = 0E)')
 plt.savefig('S_Salinity_Vertical_section_yz_cas4'+'.png')
 plt.clf()
