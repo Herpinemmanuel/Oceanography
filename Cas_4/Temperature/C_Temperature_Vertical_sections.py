acce66d2e405541a0a2050906639ec07953546f8
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-06-16 12:02:15 +0200

    Create C_Temperature_Vertical_sections.py

---
+++ Cas_4/Temperature/C_Temperature_Vertical_sections.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+from xmitgcm import open_mdsdataset 
+plt.ion()
+
+dir1 = '/homedata/bderembl/runmit/test_southatlgyre3'
+
+ds1 = open_mdsdataset(dir1,iters='all',prefix=['T'])
+
+Height = ds1.T.Z
+print(Height)
+
+nx = len(ds1.T.XC)/2
+print(nx)
+ny = len(ds1.T.YC)/2
+print(ny)
+
+nt = -1
+
+# Vertical Section of Temperature
+plt.figure(1)
+ds1['T'].where(ds1.hFacC>0)[nt,:,100,:].plot()
+plt.title('Case 4 : Temperature (t=-1 ; YC = 30S)')
+plt.savefig('T_Temperature_Vertical_section(x,z)_cas4'+'.png')
+plt.clf()
+
+plt.figure(2)
+ds1['T'].where(ds1.hFacC>0)[nt,:,:,280].plot()
+plt.title('Case 4 : Temperature (t=-1 ; XC = 0E)')
+plt.savefig('T_Temperature_Vertical_section(y,z)_cas4'+'.png')
+plt.clf()
