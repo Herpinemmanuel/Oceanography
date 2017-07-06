c33caeb070495a988e9f59014c2ad55e8c139470
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-07-06 15:26:14 +0200

    Create A_Salinity_vertical_sections_movie.py

---
+++ Cas_6/Vertical_sections/A_Salinity_vertical_sections_movie.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import cartopy.crs as ccrs 
+
+from xmitgcm import open_mdsdataset 
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+plt.ion()
+
+dir1 = '/homedata/bderembl/runmit/test_southatlgyre6'
+
+ds1 = open_mdsdataset(dir1,iters='all',prefix=['S'])
+
+Height = ds1.S.Z
+print(Height)
+nx = len(ds1.S.XC)/2
+print(nx)
+ny = len(ds1.S.YC)/2
+print(ny)
+
+nt = -1
+
+# Vertical Section of Salinity
+
+plt.figure(1)
+ds1['S'].where(ds1.hFacC>0)[nt,:,100,:].plot()
+plt.title('Case 6 : Salinity (t=-1 ; YC = 30S)')
+plt.savefig('S_Salinity_Vertical_section_xz_cas6'+'.png')
+plt.clf()
+
+plt.figure(2)
+ds1['S'].where(ds1.hFacC>0)[nt,:,:,280].plot()
+plt.title('Case 6 : Salinity (t=-1 ; XC = 0E)')
+plt.savefig('S_Salinity_Vertical_section_yz_cas6'+'.png')
+plt.clf()
