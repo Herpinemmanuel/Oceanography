2202292a473e8c0846ddcb96b7972684e16f2954
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-07-06 17:13:01 +0200

    Update A_Salinity_section_xz_movie.py

--- Cas_6/Vertical_sections/A_Salinity_section_xz_movie.py
+++ Cas_6/Vertical_sections/A_Salinity_section_xz_movie.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 from xmitgcm import open_mdsdataset 
 
-
 dir1 = '/homedata/bderembl/runmit/test_southatlgyre6'
 
 ds1 = open_mdsdataset(dir1,iters='all',prefix=['S'])
@@ -19,17 +18,19 @@ nt = 0
 while (nt < 1000) :
   nt=nt+1
   print(nt)
+  
   # Vertical Sections xz of Salinity
+
   plt.figure(1)
-  ds1['S'].where(ds1.hFacC>0)[nt,:,100,:].plot()
+  ds1['S'][nt,:,100,:].plot.pcolormesh('XC', 'YC', ax=ax,vmin=30,vmax=40,cmap='ocean');
   plt.title('Vertical Section (xz) of Salinity (YC = 30S)')
   plt.text(5,5,nt,ha='center',wrap=True)
   if (nt < 10):
-     plt.savefig('Salinity_Vertical_section_xz_Cas6-'+'00'+str(nt)+'.png')
-     plt.clf()
+      plt.savefig('Salinity_Vertical_section_xz_Cas6-'+'00'+str(nt)+'.png')
+      plt.clf()
   elif (nt > 9) and (nt < 100):
-     plt.savefig('Salinity_Vertical_section_xz_Cas6-'+'0'+str(nt)+'.png')
-     plt.clf()
+      plt.savefig('Salinity_Vertical_section_xz_Cas6-'+'0'+str(nt)+'.png')
+      plt.clf()
   else:
-     plt.savefig('Salinity_Vertical_section_xz_Cas6-'+str(nt)+'.png')
-     plt.clf()
+      plt.savefig('Salinity_Vertical_section_xz_Cas6-'+str(nt)+'.png')
+      plt.clf()
