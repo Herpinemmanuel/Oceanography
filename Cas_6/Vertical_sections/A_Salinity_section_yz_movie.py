d7c6fa4e9959810d88ff6d9304edf36ac7b6d410
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-07-11 14:14:16 +0200

    Update A_Salinity_section_yz_movie.py

--- Cas_6/Vertical_sections/A_Salinity_section_yz_movie.py
+++ Cas_6/Vertical_sections/A_Salinity_section_yz_movie.py
@@ -15,16 +15,15 @@ while (nt < 1000) :
   nt=nt+1
   print(nt)
   plt.figure(1)
-  ds1['S'].where(ds1.hFacC>0)[nt,:,:,nx].plot()
-  #plt.pcolormesh('XC','YC',,vmin=30,vmax=39)
+  ds1['S'].where(ds1.hFacC>0)[nt,:,:,nx].plot.pcolormesh('YC','Z',vmin=32,vmax=38)
   plt.text(-45,-4500,nt)
   plt.title('Vertical Section (yz) of Salinity (XC = 0E)')
   if (nt < 10):
-      plt.savefig('Salinity_Vertical_section_yz_Cas6-'+'00'+str(nt)+'.png')
-      plt.clf()
+     plt.savefig('Salinity_section_yz_Cas6-'+'00'+str(nt)+'.png')
+     plt.clf()
   elif (nt > 9) and (nt < 100):
-      plt.savefig('Salinity_Vertical_section_yz_Cas6-'+'0'+str(nt)+'.png')
-      plt.clf()
+     plt.savefig('Salinity_section_yz_Cas6-'+'0'+str(nt)+'.png')
+     plt.clf()
   else:
-      plt.savefig('Salinity_Vertical_section_yz_Cas6-'+str(nt)+'.png')
-      plt.clf()
+     plt.savefig('Salinity_section_yz_Cas6-'+str(nt)+'.png')
+     plt.clf()
