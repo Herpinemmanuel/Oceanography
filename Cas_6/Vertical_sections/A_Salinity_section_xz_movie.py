96caa32541d941a33a6ea6e7ed25a7911fa1db08
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-07-11 12:52:35 +0200

    Update A_Salinity_section_xz_movie.py

--- Cas_6/Vertical_sections/A_Salinity_section_xz_movie.py
+++ Cas_6/Vertical_sections/A_Salinity_section_xz_movie.py
@@ -8,13 +8,13 @@ dir1 = '/homedata/bderembl/runmit/test_southatlgyre6'
 
 ds1 = open_mdsdataset(dir1,iters='all',prefix=['S'])
 
+ny = 100
 nt = 0
 while (nt < 1000) :
   nt=nt+1
   print(nt)
   plt.figure(1)
-  ds1['S'].where(ds1.hFacC>0)[nt,:,100,:].plot()
-  #plt.pcolormesh('XC','YC',,vmin=30,vmax=39)
+  ds1['S'].where(ds1.hFacC>0)[nt,:,ny,:].plot.pcolormesh('XC','Z',vmin=30,vmax=39)
   plt.text(-60,-4000,nt)
   plt.title('Vertical Section (xz) of Salinity (YC = 30S)')
   if (nt < 10):
