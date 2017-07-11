b6321b5a6e09b26e162ca55dabaa7cdafdd697ba
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-07-11 14:23:31 +0200

    Update B_Temperature_section_yz_movie.py

--- Cas_6/Vertical_sections/B_Temperature_section_yz_movie.py
+++ Cas_6/Vertical_sections/B_Temperature_section_yz_movie.py
@@ -16,16 +16,15 @@ while (nt < 1000) :
   
   # Vertical Sections yz of Temperature
   plt.figure(1)
-  ds1['T'].where(ds1.hFacC>0)[nt,:,:,nx].plot()
-  #plt.pcolormesh('XC','YC','Z',vmin=30,vmax=39)
+  ds1['T'].where(ds1.hFacC>0)[nt,:,:,nx].plot.pcolormesh('YC','Z',vmin=2,vmax=30)
   plt.title('Vertical Section (yz) of Temperature (XC = 0E)')
   plt.text(-45,-4500,nt)
   if (nt < 10):
-   plt.savefig('Temperature_Vertical_section_yz_Cas6-'+'00'+str(nt)+'.png')
+   plt.savefig('Temperature_section_yz_Cas6-'+'00'+str(nt)+'.png')
    plt.clf()
   elif (nt > 9) and (nt < 100):
-   plt.savefig('Temperature_Vertical_section_yz_Cas6-'+'0'+str(nt)+'.png')
+   plt.savefig('Temperature_section_yz_Cas6-'+'0'+str(nt)+'.png')
    plt.clf()
   else:
-   plt.savefig('Temperature_Vertical_section_yz_Cas6-'+str(nt)+'.png')
+   plt.savefig('Temperature_section_yz_Cas6-'+str(nt)+'.png')
    plt.clf()
