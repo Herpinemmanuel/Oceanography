56bbb735b4d6e2e55743b7692b3998517968a192
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-07-12 17:32:23 +0200

    Update A_Okubo_weiss.py

--- Cas_6/Okubo_Weiss/A_Okubo_weiss.py
+++ Cas_6/Okubo_Weiss/A_Okubo_weiss.py
@@ -16,7 +16,9 @@ grid = xgcm.Grid(ds0)
 print(grid)
 
 # sn² = (du/dx-dv/dy)²
-Sn = (grid.diff(ds0.U.where(ds0.hFacW>0)*ds0.dyC, 'X') + grid.diff(ds0.V.where(ds0.hFacW>0)*ds0.dxC, 'Y'))/ds0.rAz
+Sn = (grid.diff(ds0.U.where(ds0.hFacW>0)*ds0.dyG, 'X') + grid.diff(ds0.V.where(ds0.hFacS>0)*ds0.dxG, 'Y'))/ds0.rA
+Sn = grid.interp(Sn ,axis='X')
+Sn = grid.interp(Sn ,axis='Y')
 Sn_carree = Sn**2
 print(Sn_carree)
 
@@ -28,21 +30,20 @@ print(Ss_carree)
 # Vorticity² = (dv/dx-du/dy)²
 Vorticity = (-grid.diff(ds0.U.where(ds0.hFacW>0)*ds0.dxC, 'Y') + grid.diff(ds0.V.where(ds0.hFacS>0)*ds0.dyC, 'X'))/ds0.rAz
 Vor_carree = Vorticity**2
+print(Vor_carree)
 
 Okubo_weiss_criterion = Sn_carree + Ss_carree - Vor_carree
+print(Okubo_weiss_criterion)
 
 i = 0
 nz = 0
-
 
 while (i < 1000) :
    i=i+1
    print(i)
    plt.figure(1)
-   #CS = plt.contour(X, Y, Z)
-   #plt.clabel(CS, inline=1, fontsize=10)
    ax = plt.subplot(projection=ccrs.PlateCarree());
-   Okubo_weiss_criterion[i,nz,:,:].plot.pcolormesh('XG','YG', ax=ax,cmap='ocean')
+   Okubo_weiss_criterion[i,nz,:,:].plot.pcolormesh('XG','YG',ax=ax,vmin=-1E-10,vmax=1E-10,cmap='seismic')
    plt.title('Case 6 : Okubo Weiss Criterion')
    plt.text(5,5,i,ha='center',wrap=True)
    ax.coastlines()
@@ -52,11 +53,11 @@ while (i < 1000) :
    gl.xformatter = LONGITUDE_FORMATTER
    gl.yformatter = LATITUDE_FORMATTER
    if (i < 10):
+     plt.savefig('Okubo_weiss_cas6-'+'000'+str(i)+'.png')
+     plt.clf()
+   elif (i >= 10) and (i < 100):
      plt.savefig('Okubo_weiss_cas6-'+'00'+str(i)+'.png')
      plt.clf()
-   elif (i > 9) and (i < 100):
-     plt.savefig('Okubo_weiss_cas6-'+'0'+str(i)+'.png')
-     plt.clf()
    else:
-     plt.savefig('Okubo_weiss_cas6-'+str(i)+'.png')
+     plt.savefig('Okubo_weiss_cas6-'+'0'+str(i)+'.png')
      plt.clf()
