1c7b24aaa75a24ec7721fd596849bde14f4ed618
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2017-07-20 17:30:48 +0200

    Create A_Diagram_Temperature_Salinity.py

---
+++ Cas_6/Diagram_TS/A_Diagram_Temperature_Salinity.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+from xmitgcm import open_mdsdataset
+
+dir1 = '/homedata/bderembl/runmit/test_southatlgyre6'
+
+ds1 = open_mdsdataset(dir1,iters='all',prefix=['S','T'])
+
+nt = 0
+
+for ny in range(0,200,40):
+    for nx in range(0,560,40):
+        print(nx)
+        print(ny)
+        plt.figure(1)
+        plt.plot(ds1['S'].where(ds1.hFacC>0)[nt,:,ny,nx],ds1['T'].where(ds1.hFacC>0)[nt,:,ny,nx])
+
+plt.title('Diagram T-S')
+plt.show()
+plt.savefig('Diagramme_T_S'+'.png')
